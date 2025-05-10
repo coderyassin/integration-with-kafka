9b24133690b989412dad8c111451e8ae2c61bdcd
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-05-10 11:55:27 +0100

    first commit

---
+++ src/main/java/org/yascode/integration_with_kafka/IntegrationWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.integration_with_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yascode/integration_with_kafka/IntegrationWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yascode.integration_with_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
